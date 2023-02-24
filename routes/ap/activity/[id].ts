d53cabc7b5b4204fa57549965ae81a079e40386c
Adam R. Nelson <adam@nels.onl>
2023-02-23 23:39:23 -0500

    fix activity urls

--- routes/ap/activity/[id].ts
+++ routes/ap/activity/[id].ts
@@ -5,10 +5,10 @@ import { LocalActivityStore } from "$/models/LocalActivity.ts";
 export const handler: Handlers<void, { injector: Injector }> = {
   async GET(_req, ctx) {
     const service = await ctx.state.injector.resolve(LocalActivityStore),
-      object = await service.get(ctx.params.id);
-    if (!object) {
+      activity = await service.get(ctx.params.id);
+    if (!activity) {
       return Response.json({ error: "Record not found" }, { status: 404 });
     }
-    return Response.json(object);
+    return Response.json(activity.json);
   },
 };
